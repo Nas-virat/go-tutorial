edaf879c441d3cb24e2a8034861e9ae6897fde90
Nas-virat <napas.vin@gmail.com>
2023-07-31 15:44:46 +0700

    test handler and comment

--- gotest/services/grade_test.go
+++ gotest/services/grade_test.go
@@ -8,6 +8,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// to create a unit test 
+// create a func "Test"
+
+// to run the unit test with command line
+// First Approch
+// cd services
+// go test -v
+
+// Second Approch
+// go test <modulename>/folder
+// in this case go test gotest/services -v
+// note that module name is in go.mod file
+
+// run unit test check cover case
+// go test gotest/services -cover
 func TestCheckGrade(t *testing.T) {
 
 	type testCase struct {
@@ -40,6 +55,18 @@ func BenchmarkCheckGrade(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		services.CheckGrade(80)
+	}
+	for i := 0; i < b.N; i++ {
+		services.CheckGrade(70)
+	}
+	for i := 0; i < b.N; i++ {
+		services.CheckGrade(60)
+	}
+	for i := 0; i < b.N; i++ {
+		services.CheckGrade(50)
+	}
+	for i := 0; i < b.N; i++ {
+		services.CheckGrade(10)
 	}
 
 }
