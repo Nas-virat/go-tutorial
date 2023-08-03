e18f989ac08bda349c040761c3b169d0c2c3207c
Nas-virat <napas.vin@gmail.com>
2023-08-04 02:56:56 +0700

    ..

--- goredis/main.go
+++ goredis/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"goredis/repositories"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	db := initDatabase()
+
+	productRepo := repositories.NewProductRepository(db)
+	products,err := productRepo.GetProduct()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(products)
+
+	/*
 	app := fiber.New()
 
 	app.Get("/hello", func(c *fiber.Ctx) error {
-		time.Sleep(time.Millisecond*10)
+		time.Sleep(time.Millisecond * 10)
 		return c.SendString("Hello World")
 	})
 
 	app.Listen(":8000")
+	*/
+}
 
+func initDatabase() *gorm.DB {
+	dial := mysql.Open("root:root@tcp(127.0.0.1:3306)/fiber?charset=utf8mb4&parseTime=True&loc=Local")
+	db, err := gorm.Open(dial, &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
--- goredis/repositories/product_db.go
+++ goredis/repositories/product_db.go
@@ -14,10 +14,17 @@ type productRepositoryDB struct {
 
 func NewProductRepository(db *gorm.DB) ProductRepository {
 	db.AutoMigrate(&product{})
-	return productRepositoryDB{db:db}
+	return productRepositoryDB{db: db}
 }
 
-func mockData(db *gorm.DB) error{
+func mockData(db *gorm.DB) error {
+
+	//set once a time
+	var count int64
+	db.Model(&product{}).Count(&count)
+	if count > 0 {
+		return nil
+	}
 
 	//random
 	seed := rand.NewSource(time.Now().UnixNano())
@@ -25,14 +32,17 @@ func mockData(db *gorm.DB) error{
 	random := rand.New(seed)
 
 	products := []product{}
-	for i:=0; i< 5000;i++{
-		products =  append(products,product{
-			Name: fmt.Sprintf("Product%v", i+1),
-			Quantity: ,
+	for i := 0; i < 5000; i++ {
+		products = append(products, product{
+			Name:     fmt.Sprintf("Product%v", i+1),
+			Quantity: random.Intn(100),
 		})
 	}
+	return db.Create(&products).Error
 }
 
-func (r productRepositoryDB) GetProduct() ([]product, error){
-	return nil,nil
+func (r productRepositoryDB) GetProduct() (products []product, err error) {
+
+	err = r.db.Order("quantity desc").Limit(30).Find(&products).Error
+	return products,err
 }
