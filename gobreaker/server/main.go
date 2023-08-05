b8108db0c474c1e918bbd764dff8f8e5e3ab0cfc
Nas-virat <napas.vin@gmail.com>
2023-08-05 15:36:25 +0700

    add breaker

--- gobreaker/server/main.go
+++ gobreaker/server/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main(){
-		
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	counter := 0
+
+	app.Get("/api", func(c *fiber.Ctx) error {
+		counter++
+		if counter >= 5 && counter <= 10 {
+			time.Sleep(1000 * time.Second)
+		}
+
+		msg := fmt.Sprintf("Hello %v", counter)
+
+		fmt.Println(msg)
+
+		return c.SendString(msg)
+	})
+	app.Listen(":8000")
+
 }
